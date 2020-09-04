f74f78363eb6d8c54a528b6ab5d1862dc71d950c
Jeffrey Bolling <j2b2590@gmail.com>
2020-09-04 15:59:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,8 +28,10 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
 
 
-  planeteer_calls.include?("#{valid_calls}")
-  
+  if planeteer_calls.include?("#{valid_calls}")
+    puts nil
+  else puts planeteer_calls[0]
+  end
 
 
 end
