8e0190161de4bb36405cedc7011bb523a7191d61
Jeffrey Bolling <j2b2590@gmail.com>
2020-09-04 16:11:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ def find_valid_calls(planeteer_calls)
 
 match = []
   planeteer_calls.each do |call|
-     if "#{call}".include?"#{valid_calls}"
+     if call.include?"#{valid_calls}"
        puts call
     else puts nil
     end
