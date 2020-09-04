eca37aa9672bf3a8f08167eb9c456cceadb74e33
Jeffrey Bolling <j2b2590@gmail.com>
2020-09-04 15:53:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
 
 
-  planeteer_calls.each.any? { |string| string == valid_calls  }
+  planeteer_calls.each { |string| string.any? == valid_calls  }
 
 
 end
