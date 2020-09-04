c86adb2ff108dd66241d17418aeb9bea1913da3d
Jeffrey Bolling <j2b2590@gmail.com>
2020-09-04 16:27:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,11 +26,5 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
-find_this = Set.new("#{planeteer_calls}")
-match = valid_calls.find{|x| find_this == Set.new(x)}
-match
+valid_calls.find_all{|match| match == "#{planeteer_calls}"}
 end
-
-
-# find_this = Set.new(a)
-# the_match = aa.find { |x| find_this == Set.new(x) }
